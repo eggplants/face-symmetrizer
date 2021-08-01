b1a0203f34877e3f962dad7dc81c96e2e52fffc0
eggplants <w10776e8w@yahoo.co.jp>
2021-08-01 09:43:49 +0900

    add: class

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup  # type: ignore
+
+from face_symmetrizer import __version__
+
+setup(
+    name='face_symmetrizer',
+    version=__version__,
+    description='Easy symmetrizer for an image contained face(s)',
+    description_content_type='',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/eggplants/',
+    author='eggplants',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    include_package_data=True,
+    license='MIT',
+    install_requires=open('requirements.txt').read().rstrip().split('\n')
+)
